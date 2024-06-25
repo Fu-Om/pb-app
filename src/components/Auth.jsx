3d991e5b3dfc132fb72bbd53d26826069881d1c5
fuuuumin <fuuuumin314@gmail.com>
2024-06-26 04:20:07 +0900

    Auto update verification using useQuery

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -2,25 +2,31 @@ import pb from "../lib/pocketbase";
 import { useForm } from "react-hook-form";
 import useLogin from "../hooks/useLogin";
 import useLogout from "../hooks/useLogout";
-import useVerified from "../hooks/useVerified";
+import useVerified, { requestVerification } from "../hooks/useVerified";
 
 const Auth = () => {
   const { mutate: login, isLoading, isError } = useLogin();
   const logout = useLogout();
-  const { isVerified, requestVerification } = useVerified();
+  const {
+    data: isVerified,
+    isLoading: isVerifiedLoading,
+    isError: isVerifiedError,
+  } = useVerified();
   const { register, handleSubmit, reset } = useForm();
   const isLoggedIn = pb.authStore.isValid;
   const onSubmit = async (data) => {
-    console.log(data.email);
     login({ email: data.email, password: data.password });
     reset();
   };
-
   if (isLoggedIn) {
     return (
       <>
         <h1>Logged In: {pb.authStore.model.email}</h1>
-        <p>Verified: {isVerified.toString()}</p>
+        {!isVerifiedLoading && !isVerifiedError ? (
+          <p>Verified: {isVerified.toString()}</p>
+        ) : (
+          <p>Verification Pending</p>
+        )}
         {!isVerified && (
           <button onClick={requestVerification}>Verify Email</button>
         )}
--- src/hooks/useVerified.js
+++ src/hooks/useVerified.js
@@ -1,25 +1,21 @@
-import { useEffect, useState } from "react";
 import pb from "../lib/pocketbase";
+import { useQuery } from "@tanstack/react-query";
 
-const useVerified = () => {
-  const [isVerified, setIsVerified] = useState(false);
-  useEffect(() => {
-    const checkVerified = async () => {
-      const id = pb.authStore.model.id;
-      const userData = await pb.collection("users").getOne(id);
-      console.log(userData);
-      setIsVerified(userData.verified);
-    };
-    const isLoggedIn = pb.authStore.isValid;
-    if (isLoggedIn) checkVerified();
-  }, []);
-
-  const requestVerification = async () => {
-    const email = pb.authStore.model.email;
-    const res = await pb.collection("users").requestVerification(email);
-    if (res) alert("Verification email sent");
+export default function useVerified() {
+  const id = pb.authStore.model?.id;
+  const checkVerified = async () => {
+    //console.log(args);
+    const userData = await pb.collection("users").getOne(id);
+    return userData.verified;
   };
-  return { isVerified, requestVerification };
-};
+  return useQuery({
+    queryFn: checkVerified,
+    queryKey: ["check-verified", id],
+  });
+}
 
-export default useVerified;
+export const requestVerification = async () => {
+  const email = pb.authStore.model.email;
+  const res = await pb.collection("users").requestVerification(email);
+  if (res) alert("Verification email sent");
+};
